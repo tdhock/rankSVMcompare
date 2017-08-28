e5d55667af0d35aaf18c7317c5b1b732cac5692a
David Venuto <dv.venuto@gmail.com>
2017-08-28 10:48:47 -0400

    Update CrossValidationLoop.R

--- R/CrossValidationLoop.R
+++ R/CrossValidationLoop.R
@@ -16,19 +16,19 @@ rankSVM_CV <-
       splits_test <- c(1 / 2, 1 / 3, 1 / 4, 1 / 5, 1 / 6)
       for (i in 1:length(C_vector)) {
         for (j in 1:length(splits_test)) {
-          Xi_train_run <- Xi_train[1:(nrow(Xi_train * splits_train[j])), ]
+          Xi_train_run <- Xi_train[1:(as.integer((nrow(Xi_train) * splits_train[j])), ]
           Xi_test_run <-
-            Xi_train[(nrow(Xi_train * splits_train[j]) + 1):(nrow(Xi_train)), ]
+            Xi_train[(as.integer(nrow(Xi_train) * splits_train[j] + 1)):(nrow(Xi_train)), ]
           
           Xip_train_run <-
-            train[1:(nrow(train * splits_train[j])), ]
+            Xi_train[1:(as.integer(nrow(Xi_train) * splits_train[j])), ]
           Xip_test_run <-
-            train[(nrow(train * splits_train[j]) + 1):(nrow(train)), ]
+            Xip_train[(as.integer(nrow(Xip_train) * splits_train[j] + 1)):(nrow(Xip_train)), ]
           
           train_yi_run <-
-            train_yi[1:(nrow(train_yi * splits_train[j])), ]
+            train_yi[1:(as.integer(nrow(train_yi) * splits_train[j])), ]
           test_yi_run <-
-            train_yi[(nrow(train_yi * splits_train[j]) + 1):(nrow(train_yi)), ]
+            train_yi[(as.integer(nrow(train_yi) * splits_train[j] + 1)):(nrow(train_yi)), ]
           
           train_pairs <-
             list(
@@ -49,7 +49,7 @@ rankSVM_CV <-
           Xi_rank <- rankdiff(qp, Xi_test_run, Xip_test_run)
           AUC <- calc_AUC(Xi_rank, test_yi_run)
           
-          store_AUC[[i]] <-
+          store_AUC[[paste(i,j)]] <-
             data.frame(TYPE = C_vector[[i]],
                       CV=splits_train[j],
                       AUC = AUC)
@@ -63,20 +63,21 @@ rankSVM_CV <-
       for (k in 1:length(sigma_vector)) {
         for (i in 1:length(C_vector)) {
           for (j in 1:length(splits_test)) {
-            Xi_train_run <- Xi_train[1:(nrow(Xi_train * splits_train[j])), ]
-            Xi_test_run <-
-              Xi_train[(nrow(Xi_train * splits_train[j]) + 1):(nrow(Xi_train)), ]
             
-            Xip_train_run <-
-              train[1:(nrow(train * splits_train[j])), ]
-            Xip_test_run <-
-              train[(nrow(train * splits_train[j]) + 1):(nrow(train)), ]
-            
-            train_yi_run <-
-              train_yi[1:(nrow(train_yi * splits_train[j])), ]
-            test_yi_run <-
-              train_yi[(nrow(train_yi * splits_train[j]) + 1):(nrow(train_yi)), ]
-            
+          Xi_train_run <- Xi_train[1:(as.integer((nrow(Xi_train) * splits_train[j])), ]
+          Xi_test_run <-
+            Xi_train[(as.integer(nrow(Xi_train) * splits_train[j] + 1)):(nrow(Xi_train)), ]
+          
+          Xip_train_run <-
+            Xi_train[1:(as.integer(nrow(Xi_train) * splits_train[j])), ]
+          Xip_test_run <-
+            Xip_train[(as.integer(nrow(Xip_train) * splits_train[j] + 1)):(nrow(Xip_train)), ]
+          
+          train_yi_run <-
+            train_yi[1:(as.integer(nrow(train_yi) * splits_train[j])), ]
+          test_yi_run <-
+            train_yi[(as.integer(nrow(train_yi) * splits_train[j] + 1)):(nrow(train_yi)), ]
+          
             train_pairs <-
               list(
                 Xi = Xi_train_run,
@@ -94,12 +95,12 @@ rankSVM_CV <-
               softCompareQP(pairs,
                             C = C_vector[[i]],
                             kernel = kern,
-                            sigma = sigma_vector[[k]])
+                            degree = sigma_vector[[k]])
             
             Xi_rank <- rankdiff(qp, Xi_test_run, Xip_test_run)
             AUC <- calc_AUC(Xi_rank, test_yi_run)
             
-            store_AUC[[i]] <-
+            store_AUC[[paste(i,k,j)]] <-
               data.frame(TYPE = paste(sigma_vector[[k]],C_vector[[i]], sep = "_"),
                          CV=splits_train[j],
                          AUC = AUC)
