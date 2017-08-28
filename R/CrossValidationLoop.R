0165f576afdbdf32da51c06dd1bc510ffc9b5074
David Venuto <dv.venuto@gmail.com>
2017-08-28 12:35:01 -0400

    Update CrossValidationLoop.R

--- R/CrossValidationLoop.R
+++ R/CrossValidationLoop.R
@@ -3,46 +3,52 @@ rankSVM_CV <-
            Xip_train,
            train_yi,
            parameter.df,
-           kern) {
+           kern,
+           splits_train) {
     
     #Cross validation loop using different kernels
     #The Xi_train,Xip_train and train_yi arguments are ordered by event date,  First element is oldest event, last element is newest
     
-    if (kern == "vanilladot") {
-      kern.fun <- kernlab::vanilladot()
-    } else if (kern == "rbfdot") {
-      kern.fun <- kernlab::rbfdot()
-    } else if (kern == "rbfdot") {
-      kern.fun <- kernlab::polydot()
-    } else{
-      return(0)
+    kern.fun <- if(is.character(kern)){
+      get(kern)
+    }else if(is.function(kern)){
+      kern.fun
+    }else{
+      stop("kern must be either character or function")
     }
     
     if(is.null(parameter.df$C)){
-      return(0)
+      stop("C must not be null")
     }
   
     store_AUC <- list()
-    splits_train <- c(1 / 2, 2 / 3, 3 / 4, 4 / 5, 5 / 6)
-    splits_test <- c(1 / 2, 1 / 3, 1 / 4, 1 / 5, 1 / 6)
+    
+    
+    c.col.index <- which(names(parameter.df)=="C")
     
     for (combination.i in 1:nrow(parameter.df)) {
-      for (j in 1:length(splits_test)) {
-        one.combination <- parameter.df[combination.i, ]
+      one.combination <- parameter.df[combination.i, ]
+      kern.params <- one.combination[, -c.col.index]
+      C.param <- one.combination[["C"]]
+      svm_kern <- do.call(kern.fun, kern.params)
+      for (split.i in 1:length(splits_train)) {
+        
+        rows_train <- 1:(as.integer(nrow(Xi_train) * splits_train[split.i]))
+        rows_validation <- -rows_train
         Xi_train_run <-
-          Xi_train[1:(as.integer(nrow(Xi_train) * splits_train[j])),]
-        Xi_test_run <-
-          Xi_train[(as.integer(nrow(Xi_train) * splits_train[j] + 1)):(nrow(Xi_train)),]
+          Xi_train[rows_train,]
+        Xi_validation_run <-
+          Xi_train[rows_validation,]
         
         Xip_train_run <-
-          Xi_train[1:(as.integer(nrow(Xi_train) * splits_train[j])),]
-        Xip_test_run <-
-          Xip_train[(as.integer(nrow(Xip_train) * splits_train[j] + 1)):(nrow(Xip_train)),]
+          Xi_train[rows_train,]
+        Xip_validation_run <-
+          Xip_train[rows_validation,]
         
         train_yi_run <-
-          train_yi[1:(as.integer(nrow(train_yi) * splits_train[j])),]
-        test_yi_run <-
-          train_yi[(as.integer(nrow(train_yi) * splits_train[j] + 1)):(nrow(train_yi)),]
+          train_yi[rows_train,]
+        validation_yi_run <-
+          train_yi[rows_validation,]
         
         train_pairs <-
           list(
@@ -50,22 +56,16 @@ rankSVM_CV <-
             Xip = Xip_train_run,
             yi = train_yi_run
           )
-        test_pairs <-
-          list(
-            Xi = Xi_test_run,
-            Xip = Xip_test_run,
-            yi = test_yi_run
-          )
         
-        qp <- do.call(softCompareQP,args=c(Pairs=train_pairs,one.combination,kernel=kern))
+        qp <- softCompareQP(train_pairs,svm_kern)
         
-        Xi_rank <- rankdiff(qp, Xi_test_run, Xip_test_run)
-        AUC <- calc_AUC(Xi_rank, test_yi_run)
+        Xi_rank <- rankdiff(qp, Xi_validation_run, Xip_validation_run)
+        AUC <- calc_AUC(Xi_rank, validation_yi_run)
         
-        store_AUC[[paste(i, j)]] <-
+        store_AUC[[paste(combination.i, split.i)]] <-
           data.frame(
             TYPE = paste(one.combination, collapse = "_"),
-            CV = splits_train[j],
+            CV = splits_train[split.i],
             AUC = AUC
           )
       }
