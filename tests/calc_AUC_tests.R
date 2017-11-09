fa968107319d126d4b0603961e9b4b025fa54de1
David Venuto <dv.venuto@gmail.com>
2017-11-09 15:31:52 -0500

    Update calc_AUC_tests.R

--- tests/calc_AUC_tests.R
+++ tests/calc_AUC_tests.R
@@ -1,12 +1,28 @@
-## TESTS
+## TESTS calcAUC
 test_rankdiff_vector <- c(0.1, 0.2, -0.3, 0.4)
 test_labels <- c(1, 0, -1, 0)
-if (0.25 != calc_AUC(test_rankdiff_vector, test_labels)) {
+AUC_test <- calc_AUC(test_rankdiff_vector, test_labels)
+
+if (AUC_test != 0.25) {
   stop()
 }
 
 test_rankdiff_vector <- c(0,1)
 test_labels <- c(0,1)
-if (1 != calc_AUC(test_rankdiff_vector, test_labels)) {
+AUC_test <- calc_AUC(test_rankdiff_vector, test_labels)
+if (AUC_test != 1) {
   stop()
 }
+
+# TESTS Baseline
+test_labels <- c(1,0,-1,0,1,1)
+AUC_test <- calc_Baseline(test_labels)
+if(AUC_test != 0.375){
+  stop()
+}
+
+
+
+
+
+
