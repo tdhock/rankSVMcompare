51fc21eea7b206b5be600deadf81c786c517efbd
David Venuto <dv.venuto@gmail.com>
2017-11-09 15:25:15 -0500

    Create calc_Baseline.R

---
+++ R/calc_Baseline.R
@@ -0,0 +1,9 @@
+calc_Baseline <- function(labls) {
+  most_common <- 1
+  if (length(labls[labls == -1]) > length(labls[labls == 1])) {
+    most_common <- -1
+  }
+  ROC <- data.frame(FPR=c(0,1),TPR=c(0,length(labls[labls==most_common])/length(labls[labls==-1 | labls == 1])))
+  trapz(x=ROC$FPR,y=ROC$TPR)
+  return(ROC)
+}
