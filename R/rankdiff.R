0ba9647ab42a5f08d9d91d48933271a78e9056c0
David Venuto <dv.venuto@gmail.com>
2017-11-09 14:50:47 -0500

    Update rankdiff.R
    We can change this one later but it is not too slow.

--- R/rankdiff.R
+++ R/rankdiff.R
@@ -1,13 +1,23 @@
-rankdiff <- function(qp, matrix_Xi, matrix_Xip) {
-
-  #Computes the rankdiff given rsvm objects  
-
-  Xirank <- qp$rank.scaled(X = matrix_Xi)
-  Xiprank <- qp$rank.scaled(X = matrix_Xip)
-  
+rankdiff <- function(qp, matrix_Xi, matrix_Xip, t) {
+  Xirank <- qp$rank(X = matrix_Xi)
+  Xiprank <- qp$rank(X = matrix_Xip)
   Xirank <- cbind(Xirank, Xiprank)
-  Xirank <- data.frame(Xirank)
   
-  Xirank$difference <- abs(Xirank$X2 - Xirank$X1)
+  Xirank <- data.frame(Xirank)
+  Xirank$diff <- Xirank[, 2] - Xirank[, 1]
+  Xirank$label <- NA
+  
+  for (i in 1:nrow(Xirank)) {
+    diff <- Xirank[i, 2] - Xirank[i, 1]
+    if (diff < (-1 * t)) {
+      Xirank[i, 4] <- -1
+    }
+    if (diff > t) {
+      Xirank[i, 4] <- 1
+    }
+    if (abs(diff) <= t) {
+      Xirank[i, 4] <- 0
+    }
+  }
   return(Xirank)
 }
