e183e1d12b15663a2f9fffe8ee91bbd414384948
David Venuto <dv.venuto@gmail.com>
2017-11-09 15:31:11 -0500

    Update calc_Baseline.R

--- R/calc_Baseline.R
+++ R/calc_Baseline.R
@@ -4,6 +4,6 @@ calc_Baseline <- function(labls) {
     most_common <- -1
   }
   ROC <- data.frame(FPR=c(0,1),TPR=c(0,length(labls[labls==most_common])/length(labls[labls==-1 | labls == 1])))
-  trapz(x=ROC$FPR,y=ROC$TPR)
-  return(ROC)
+  AUC <- trapz(x=ROC$FPR,y=ROC$TPR)
+  return(AUC)
 }
