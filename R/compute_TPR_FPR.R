5eaf905c1b0c29c97c53a3ea860e15621e53d876
David Venuto <dv.venuto@gmail.com>
2017-11-09 14:27:06 -0500

    Create compute_TPR_FPR.R

---
+++ R/compute_TPR_FPR.R
@@ -0,0 +1,18 @@
+compute_FP_threshold <- function(rankdiff_vector,labls){
+  rd<- data.table(diff=rankdiff_vector, label=labls)
+  rd[, abs.diff := abs(diff)]
+  rd[order(-abs.diff)]
+  rd[, FP := cumsum(label==0)]
+  rd[, FPR := FP / sum(label != 0)]
+  return(rd)
+}
+
+compute_TP_threshold <- function(rankdiff_vector,labls){
+  rd<- data.table(diff=rankdiff_vector, label=labls)
+  rd[, abs.diff := abs(diff)]
+  rd[order(-abs.diff)]
+  rd[, TP := cumsum((label==-1 & diff <  0) | (label == 1 & diff > 0))]
+  rd[, TPR := TP / sum(label != 0)]
+  rd <- rbind(data.table(diff=0,label=0,abs.diff=0,TP=0,TPR=0),rd)
+  return(rd)
+}
