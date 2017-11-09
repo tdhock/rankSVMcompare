aab910115d82811ce4701ff53c40ca410cebc8b3
David Venuto <dv.venuto@gmail.com>
2017-11-09 15:24:52 -0500

    Create rankdiff_qp.R

---
+++ rankdiff_qp.R
@@ -0,0 +1,23 @@
+rankdiff_qp <- function(qp, matrix_Xi, matrix_Xip, t) {
+  Xirank <- qp$rank(X = matrix_Xi)
+  Xiprank <- qp$rank(X = matrix_Xip)
+  Xirank <- cbind(Xirank, Xiprank)
+  
+  Xirank <- data.frame(Xirank)
+  Xirank$diff <- Xirank[, 2] - Xirank[, 1]
+  Xirank$label <- NA
+  
+  for (i in 1:nrow(Xirank)) {
+    diff <- Xirank[i, 2] - Xirank[i, 1]
+    if (diff < (-1 * t)) {
+      Xirank[i, 4] <- -1
+    }
+    if (diff > t) {
+      Xirank[i, 4] <- 1
+    }
+    if (abs(diff) <= t) {
+      Xirank[i, 4] <- 0
+    }
+  }
+  return(Xirank)
+}
