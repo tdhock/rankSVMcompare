3d71589378ec79faa9607eecf954579fa2fae8a5
David Venuto <dv.venuto@gmail.com>
2017-11-10 11:17:58 -0500

    Update for_server.R

--- R/for_server.R
+++ R/for_server.R
@@ -53,15 +53,6 @@ rankdiff <- function(rank, rank_p, t) {
   return(Xirank)
 }
 
-compute_FP_threshold <- function(rankdiff_vector, labls) {
-  rd <- data.table(diff = rankdiff_vector, label = labls)
-  rd[, abs.diff := abs(diff)]
-  ord <- rd[order(-abs.diff)]
-  ord[, FP := cumsum(label == 0)]
-  ord[, FPR := FP / sum(label != 0)]
-  return(rd)
-}
-
 compute_threshold <- function(rankdiff_vector, labls) {
   rd <- data.table(diff = rankdiff_vector, label = labls)
   rd[, abs.diff := abs(diff)]
